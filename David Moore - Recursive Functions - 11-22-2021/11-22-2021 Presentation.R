5635211789a196213ce96036d1d9d3b20eca408a
David Moore <46967676+davidblakneymoore@users.noreply.github.com>
2021-11-22 16:31:56 -0500

    Update 11-22-2021 Presentation.R

--- David Moore - Recursive Functions - 11-22-2021/11-22-2021 Presentation.R
+++ David Moore - Recursive Functions - 11-22-2021/11-22-2021 Presentation.R
@@ -8,13 +8,78 @@
 # functions a few years ago when he came to
 # present at an R Coding Club meeting.
 
-# A recursive function is a function that calls itself.
+# Before we start, there are just a few more
+# things I want to show you before we write these
+# recursive functions.
+
+
+# Warning Messages
+
+# We can write warning messages into our custom
+# functions.
+
+Function_1 <- function (x) {
+  if (x == 1) {
+    warning ("'x' is equal to 1 and it won't change when you square it!")
+  }
+  x ^ 2
+}
+Function_1(2)
+Function_1(1)
+
+# Error Messages
+
+# We can also write error messages into our
+# custom functions.
+
+Function_2 <- function (x) {
+  if (x == 0) {
+    stop ("You can't divide a number by 0!")
+  }
+  1 / x
+}
+Function_2(2)
+Function_2(0)
+
+# The difference between a warning message and an
+# error message is that a warning message does
+# not stop the function from running - the
+# function still runs, but it tells you something
+# may be fishy. With an error message, the
+# function does not run.
+
+
+# Quotation Marks in Character Strings
+
+# There's one other small detail you may have
+# noticed - I nested single quotation marks
+# inside double quotation marks. This is how you
+# get quotation marks to show up as quotation
+# marks inside a character string without using
+# regular expressions, which we'll talk about
+# soon.
+
+# You can also do the reverse (nest double
+# quotation marks inside single quotation
+# marks).
+
+(Character_String_1 <- "My dog's name is Sally.")
+(Character_String_2 <- 'Bob said, "please sit down."')
+
+# We'll talk more about these backslashes when we
+# talk about regular expressions.
+
+
+# Recursive Functions
+
+# A recursive function is a function that calls
+# itself.
 
 # Let's write a function to calculate the nth
 # term of the Fibonacci sequence.
 
 Fibonacci_Sequence_Function <- function (x) {
-  if (x %% 1 != 0 | x < 1 | length(x) != 1 | !is.numeric(x)) {
+  if ((x %% 1 != 0) | (x < 1) | (length(x) != 1) | (!is.numeric(x))) {
     stop ("'x' is not a positive integer.")
   }
   if (x == 1) {
